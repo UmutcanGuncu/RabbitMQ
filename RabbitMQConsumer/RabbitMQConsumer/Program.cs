2ff24d90af99d86d580bbe74dc07903dad1a73bb
umutcanguncu <umutcanguncu16@gmail.com>
2025-02-23 19:46:18 +0300

    first commit

---
+++ RabbitMQConsumer/RabbitMQConsumer/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+//Bağlantı oluşturma
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rvvkxluh:xZyCL6Ea-2UgGTTGgfLnOSRyXyiZnXN3@hawk.rmq.cloudamqp.com/rvvkxluh");
+
+//Bağlantı AKtifleştirme ve Kanal AÇma
+using IConnection connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+//Queue oluşturma
+await channel.QueueDeclareAsync(queue:"umut",exclusive:false,autoDelete: false,durable:true); //Consumerda' da kuyruk publishe ile birebir aynı yapıda tanımlanmalıdır
+
+//Queue'den Mesaj Okuma
+AsyncEventingBasicConsumer consumer = new AsyncEventingBasicConsumer(channel);
+await channel.BasicConsumeAsync(queue:"umut", autoAck: false, consumer: consumer); //auto ack parametresi ile mesajın kuyruktan silinmesi için consumer'dan onay bekleyecektir
+//Fair Dispatch davranışı konfigürasyonu
+await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
+//prefetchSize : consumer tarafından alınabilecek en büyük mesaj boyutunu byte cinsinden belirler. 0 sınırsız demek
+//prefetchCount: Consumer tarafından aynı anda işleme alınabilecek mesaj sayısı
+//global: Tüm consumerlar için mi yoksa çağrı yapılan ilgili consumer için mi geçerli olduğunu belirler
+consumer.ReceivedAsync += async (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    
+    await channel.BasicAckAsync(e.DeliveryTag, false); // işlenen bu mesajın silineceğini bildiririz 
+    //multipile a false dediğimizde sadece bu mesaja dair bir çalışma yapacağımı bildiririz
+    
+};
+
+Console.ReadLine();
---
+++ RabbitMqPublisher/RabbitMqPublisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rvvkxluh:xZyCL6Ea-2UgGTTGgfLnOSRyXyiZnXN3@hawk.rmq.cloudamqp.com/rvvkxluh");
+
+//Bağlantıyı Aktifleştirme ve Kanal Açma
+using IConnection connection =  await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+//Queue oluşturma
+await channel.QueueDeclareAsync(queue:"umut",exclusive:false,autoDelete: false,durable:true); // Mesajların sunucudaki bir sorun nedeniyle kaybolmasını önlemek için 
+//durability parametresini true yapmalıyız
+
+//Queue'ya Mesaj Gönderme
+//Byte türünde mesaj kabul etmektedir. Bu yüzden byte'a dönüştürmemiz gerekmektedir.
+var props = new BasicProperties(); //BasicProperties sınıfından yeni nesne oluştur
+props.Persistent = true; //Parametresini true yap
+byte [] message = Encoding.UTF8.GetBytes("Selam");  
+await channel.BasicPublishAsync(exchange:"",routingKey:"umut",body:message, basicProperties:props, mandatory: true); //oluşturduğumuz props u uygun parametreye veririz
+//Manndorty ise routing key de verilen isimle uygun bir kuyruk bulamazsa mesajın kaybolmasını engeller true yapılırsa
+
+Console.Read();
